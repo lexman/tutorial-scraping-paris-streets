9b1e8ed352ad81b6db04f753deb8546f2b061a72
lexman <android@lexman.org>
2019-04-13 15:34:06 +0200

    Listed all urls with streets of Paris

--- src/scrapper.py
+++ src/scrapper.py
@@ -17,8 +17,8 @@ def clean_comment(name_with_parenthesis):
 def find_all_streets(html):
     soup = BeautifulSoup(html)
     titles = soup.find_all("h2")
-    assert titles[0].text.startswith("Liste")
-    assert titles[1].text.startswith("Voir aussi")
+    assert titles[0].text.startswith("Liste"), titles[0].text
+    assert titles[1].text.startswith("Voir aussi"), titles[1].text
     all_li = titles[1].find_all_previous("li")
     labels = [clean_comment(li.text) for li in all_li]
     return labels
@@ -28,20 +28,43 @@ def streets_from(url, arrondissement):
     html = from_page(url)
     result = [(street, arrondissement, url) for street in find_all_streets(html)]
     return result
-    
 
-paris_1_url = (
-    "https://fr.wikipedia.org/wiki/Liste_des_voies_du_1er_arrondissement_de_Paris"
-)
 
+urls = [
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_1er_arrondissement_de_Paris", 1),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_2e_arrondissement_de_Paris", 2),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_3e_arrondissement_de_Paris", 3),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_4e_arrondissement_de_Paris", 4),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_5e_arrondissement_de_Paris", 5),
+#    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_6e_arrondissement_de_Paris", 6),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_7e_arrondissement_de_Paris", 7),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_8e_arrondissement_de_Paris", 8),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_9e_arrondissement_de_Paris", 9),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_10e_arrondissement_de_Paris", 10),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_11e_arrondissement_de_Paris", 11),
+#    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_12e_arrondissement_de_Paris", 12),
+#    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_bois_de_Vincennes", 12),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_13e_arrondissement_de_Paris", 13),
+#    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_14e_arrondissement_de_Paris", 14),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_15e_arrondissement_de_Paris", 15),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_16e_arrondissement_de_Paris", 16),
+#    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_bois_de_Boulogne", 16),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_17e_arrondissement_de_Paris", 17),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_18e_arrondissement_de_Paris", 18),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_19e_arrondissement_de_Paris", 19),
+    ("https://fr.wikipedia.org/wiki/Liste_des_voies_du_20e_arrondissement_de_Paris", 20),
+]
 
 def to_csv(table):
     res = "street,arrondissement_number,from_url\n"
     for (street, arrondissement, from_url) in table:
         res += "{},{},{}\n".format(street, arrondissement, from_url)
     return res
-    
-table = streets_from(paris_1_url, 1)
+
+table = []
+for (url, num_arrondissement) in urls:    
+    print("Scraping {}\n".format(url))
+    table += streets_from(url, num_arrondissement)
 csv = to_csv(table)
 with open("paris_streets.csv", "w") as f:
     f.write(csv)
